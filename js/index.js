7517a192d345241740af76ab0517d2bf71760869
juratbek <juratbekraxmatov515@gmail.com>
2022-12-24 17:36:41 +0500

    jaas

---
+++ js/index.js
@@ -0,0 +1,2 @@
+"use strict";
+const root = document.getElementById('root');
---
+++ ts/createElement.ts
@@ -0,0 +1,11 @@
+
+type AttributeType = {
+   name: string
+   value: string
+}
+
+function createElement(tagName: string, attributes: AttributeType[]){
+
+}
+
+export  default createElement
